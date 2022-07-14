fb6198018d720018601ed3eaa1cb8dad7f334f4c
user-vsssfurg <107590981+user-vsssfurg@users.noreply.github.com>
2022-07-14 20:41:43 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
-use fbot_vss_rust::fbot_fira::{fira_protos, get_ball, get_yellow_robot, Robot, Teams};
-
-
+use fbot_vss_rust::fbot_fira::{fira_protos, get_ball, get_yellow_robot, Robot, Team};
+use flo_curves::{bezier::Curve, Coord2, BezierCurve};
 
 fn main() {
     loop {
-        let goalie = Robot::new(0, Teams::Yellow);
-        println!("{:?}", goalie);
-        // let ball = get_ball();
+        let goalie = Robot::new(0, Team::Yellow);
+        let ball = get_ball();
 
-        println!("{:?}", goalie.get_x());
-        // goalie.go_to(ball.x, ball.y)
+        let curve = Curve {
+            start_point: Coord2 (goalie.get_x(), goalie.get_y()),
+            end_point: Coord2 (ball.x, ball.y),
+            control_points: (Coord2 (0.0, 0.0), Coord2 (0.0, 0.0) )
+        };
+
+
+        println!("{:?}", curve.point_at_pos(0.1));
+
     }
 }
