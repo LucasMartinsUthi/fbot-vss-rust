b0d81ca98b0fc33ad2b937a24931190b6afa062e
LucasMartinsUthi <luscagbr@gmail.com>
2022-07-20 00:42:50 -0300

    ball struct

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
-use fbot_vss_rust::fbot_fira::{fira_protos, get_ball, get_yellow_robot, Robot, Team, get_ball_cp};
+use fbot_vss_rust::{Robot, Team, Ball};
 use flo_curves::{bezier::Curve, Coord2, BezierCurve};
 use std::{thread, time};
 
 fn main() {
     let goalie = Robot::new(0, Team::Yellow);
-    let ball = get_ball();
+    let ball = Ball::new();
 
-    let cp_goalie = goalie.get_control_point();
-    let cp_ball = get_ball_cp();
+    let cp_goalie = goalie.control_point();
+    let cp_ball = ball.control_point();
 
 
     let curve = Curve {
-        start_point: Coord2 (goalie.get_x(), goalie.get_y()),
-        end_point: Coord2 (ball.x, ball.y),
+        start_point: Coord2 (goalie.x(), goalie.y()),
+        end_point: Coord2 (ball.x(), ball.y()),
         control_points: (Coord2 (cp_goalie.0, cp_goalie.1), Coord2 (cp_ball.0, cp_ball.1))
     };
 
@@ -27,7 +27,6 @@ fn main() {
 
     goalie.set_speed(100.0, 100.0);
 
-    
     thread::sleep(time::Duration::from_millis(300));
 
     goalie.set_speed(0.0, 0.0);
